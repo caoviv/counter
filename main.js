4dc3cd27809f8cc27631816ee3a4155b4acf2dcf
VivianC <viviancao58@gmail.com>
2022-11-07 17:21:25 +1100

    add countValue colour change to function

--- main.js
+++ main.js
@@ -8,12 +8,27 @@ const btns = document.querySelectorAll('.btn');
 btns.forEach(function(btn) {
     btn.addEventListener('click', function(e) {
         const styles = e.currentTarget.classList;
+        // decrease countValue 
         if (styles.contains('decrease')) {
             countValue --;
+        // increase countValue 
         } else if (styles.contains('increase')) {
             countValue ++;
+        // reset countValue 
         } else {
             countValue = 0;
+        }
+        // change positive numbers to green
+        if (countValue > 0) {
+            value.style.color = 'green';
+        }
+        // change negative numbers to red
+        if (countValue < 0) {
+            value.style.color = 'red';
+        }
+        // change number 0 to black
+        if (countValue === 0) {
+            value.style.color = '#222';
         }
         value.textContent = countValue;
     })
